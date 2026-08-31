bede4136871eccaa5611e0db6743b28c4763acee
WattIQs <amaorbelo@gmail.com>
2026-08-31 01:33:30 -0300

    feat: configure TanStack Start server runtime

---
+++ app/src/start.ts
@@ -0,0 +1,5 @@
+import { createStart, createCsrfMiddleware } from '@tanstack/react-start';
+
+const csrfMiddleware = createCsrfMiddleware({ filter: (ctx) => ctx.handlerType === 'serverFn' });
+
+export const startInstance = createStart(() => ({ requestMiddleware: [csrfMiddleware] }));
